326e0373a73e2d48c7bcf4f1b185b631d25ccf49
James Adam <james@lazyatom.com>
2009-10-04 02:54:26 +0800

    We can rely on the json gem, but we should only require the Rails 'fix' if Rails is present, and actually needs it.

--- lib/contacts/gmail.rb
+++ lib/contacts/gmail.rb
@@ -1,6 +1,10 @@
 begin
   # If the json gem is available, use it
-  require "json/add/rails"
+  require 'json'
+  if Object.const_defined?(:Rails)
+    # Only 'fix' Rails' JSON behaviour if it needs fixing.
+    require "json/add/rails" unless Rails::VERSION::MAJOR >= 2 && Rails::VERSION::MINOR >= 3
+  end
 rescue MissingSourceFile
   # Otherwise wrap the ActiveSupport JSON implementation for our simple use case
   class JSON
