54937dc1de5e771256d7941c06322d3601e4c194
Ahmad Abdel-Yaman <ahmad@boundlessdrop.com>
2010-07-15 17:16:09 +0800

    Moved the 'json_picker' requirement to the top to allow the rest of the files to see it

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -3,9 +3,9 @@ $:.unshift(File.dirname(__FILE__)+"/contacts/")
 require 'rubygems'
 
 require 'base'
+require 'json_picker'
 require 'gmail'
 require 'hotmail'
 require 'yahoo'
 require 'plaxo'
 require 'aol'
-require 'json_picker'
