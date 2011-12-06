7ac3c9605bd5440c4f18881a8604c556050fce3c
ccocchi <cocchi.c@gmail.com>
2011-12-06 23:16:20 +0100

    Use nokogiri instead of hpricot with Gmail

--- lib/contacts/gmail.rb
+++ lib/contacts/gmail.rb
@@ -2,34 +2,33 @@ require 'gdata'
 
 class Contacts
   class Gmail < Base
-    
+
     CONTACTS_SCOPE = 'http://www.google.com/m8/feeds/'
     CONTACTS_FEED = CONTACTS_SCOPE + 'contacts/default/full/?max-results=1000'
-    
+
     def contacts
       return @contacts if @contacts
     end
-    
+
     def real_connect
       @client = GData::Client::Contacts.new
       @client.clientlogin(@login, @password, @captcha_token, @captcha_response)
-      
-      feed = @client.get(CONTACTS_FEED).to_xml
-      
-      @contacts = feed.elements.to_a('entry').collect do |entry|
-        title, email = entry.elements['title'].text, nil
-        entry.elements.each('gd:email') do |e|
-          email = e.attribute('address').value if e.attribute('primary')
-        end
-        [title, email] unless email.nil?
-      end
-      @contacts.compact!
+
+      feed = Nokogiri::XML(@client.get(CONTACTS_FEED).body)
+
+      @contacts = feed.root.elements.select {|n| n.name == 'entry' }.map do |node|
+        children = node.children
+        name = children.search('title').text
+        email = children.last.attr('address')
+
+        [name, email] unless name.empty? || email.empty?
+      end.compact
     rescue GData::Client::AuthorizationError => e
       raise AuthenticationError, "Username or password are incorrect"
     end
-    
+
     private
-    
+
     TYPES[:gmail] = Gmail
   end
 end
